57c2e1d14a000e8cefbd200a67924c13a340ccc7
JiangKui <44607880@qq.com>
2017-05-18 10:37:36 +0800

    organization 建立

--- organization/models.py
+++ organization/models.py
@@ -1,5 +1,12 @@
+#_*_ encoding:utf-8 _*_
+
 from __future__ import unicode_literals
+from datetime import datetime
 
 from django.db import models
 
 # Create your models here.
+
+
+class CourseOrg(models.Model):
+    

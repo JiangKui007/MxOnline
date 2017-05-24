cfda81486a7829907b956583ace382ce2de7be35
JiangKui <44607880@qq.com>
2017-05-24 19:00:57 +0800

    安装静态数据

--- apps/users/adminx.py
+++ apps/users/adminx.py
@@ -7,12 +7,25 @@ import xadmin
 
 from .models import EmailVerifyRecord
 from .models import Banner
+from xadmin import views
+
+
+class BaseSetting (object):
+    enable_themes = True
+    use_bootswatch = True
+
+
+class GlobalSetting(object):
+    site_title = "慕学后台"
+    site_footer = "云中墨客"
+    menu_style = "accordion"
 
 
 class EmailVerifyRecordAdmin(object):
     list_display = ['code','email','send_type','send_time']
     search_fields = ['code','email','send_type','send_time']
     list_filter = ['code','email','send_type','send_time']
+
 
 class BannerAdmin(object):
     list_display = ['title', 'image', 'url', 'index','add_time',]
@@ -22,3 +35,5 @@ class BannerAdmin(object):
 
 xadmin.site.register(EmailVerifyRecord, EmailVerifyRecordAdmin)
 xadmin.site.register(Banner, BannerAdmin)
+xadmin.site.register(views.BaseAdminView, BaseSetting)
+xadmin.site.register(views.CommAdminView, GlobalSetting)

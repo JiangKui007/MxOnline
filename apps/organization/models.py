2830db9d32d6799060af348c12021e40e07f9ff9
JiangKui <44607880@qq.com>
2017-06-07 23:39:04 +0800

    修改了organization的数据库模板文件，上传了新的图片7-3

--- apps/organization/models.py
+++ apps/organization/models.py
@@ -17,6 +17,9 @@ class CityDict(models.Model):
         verbose_name = u"城市"
         verbose_name_plural = verbose_name
 
+    def __unicode__(self):
+        return self.name
+
 
 class CourseOrg(models.Model):
     name = models.CharField(max_length=50,verbose_name=u"机构名称")
@@ -26,7 +29,7 @@ class CourseOrg(models.Model):
     image = models.ImageField(upload_to="org/%Y/%m", verbose_name=u"封面图")
     address = models.CharField(max_length=150, verbose_name=u"机构地址")
     city = models.ForeignKey(CityDict,verbose_name=u"所在城市")
-    add_time = models.DateTimeField(default=datetime.now)
+    add_time = models.DateTimeField(default=datetime.now, verbose_name=u"添加时间")
 
     class Meta:
         verbose_name = u"课程机构"
@@ -42,8 +45,10 @@ class Teacher(models.Model):
     points = models.CharField(max_length=50,verbose_name=u"教学特点")
     click_nums = models.IntegerField(default=0, verbose_name=u"点击数")
     fav_nums = models.IntegerField(default=0, verbose_name=u"收藏数")
-    add_time = models.DateTimeField(default=datetime.now)
+    add_time = models.DateTimeField(default=datetime.now, verbose_name=u"添加时间")
 
     class Meta:
         verbose_name = u"教师"
         verbose_name_plural = verbose_name
+
+

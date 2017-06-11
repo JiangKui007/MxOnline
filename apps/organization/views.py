683aaa2047ae0971cb8e7f8631c869fa44207c26
JiangKui <44607880@qq.com>
2017-06-11 14:51:18 +0800

    排序功能，前后端交互实现

--- apps/organization/views.py
+++ apps/organization/views.py
@@ -13,10 +13,31 @@ class OrgView(View):
     def get(self,request):
         #课程机构
         all_orgs = CourseOrg.objects.all()
-        org_nums = all_orgs.count()
+        hot_orgs = all_orgs.order_by("-click_nums")[:3]
 
         #城市
         all_citys = CityDict.objects.all()
+
+        #取出筛选城市
+        city_id = request.GET.get('city',"")
+        if city_id:
+            all_orgs = all_orgs.filter(city_id=int(city_id))
+
+        #类别筛选
+        category = request.GET.get("ct","")
+        if category:
+            all_orgs = all_orgs.filter(category=category)
+
+        #排序
+        sort = request.GET.get('sort',"")
+        if sort:
+            if sort == "students":
+                all_orgs = all_orgs.order_by("-students")
+            elif sort == "courses":
+                all_orgs = all_orgs.order_by("-course_nums")
+
+
+        org_nums = all_orgs.count()
 
         #对课程机构进行分页
         try:
@@ -28,9 +49,13 @@ class OrgView(View):
 
         orgs = p.page(page)
         return render(request, "org-list.html", {
-           "all_orgs":orgs,
+            "all_orgs":orgs,
             "all_citys":all_citys,
-            "org_nums":org_nums
+            "org_nums":org_nums,
+            "city_id":city_id,
+            "category":category,
+            "hot_orgs":hot_orgs,
+            "sort":sort,
         })
 
 

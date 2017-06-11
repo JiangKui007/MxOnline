aac5c25e1ab4c5a813375ebc688ba8814238bbf2
JiangKui <44607880@qq.com>
2017-06-11 16:57:10 +0800

    提交咨询表单的ajax设置，尚有一些小问题。
    TODO:Debug ajax设置

--- apps/organization/forms.py
+++ apps/organization/forms.py
@@ -8,7 +8,7 @@ from operation.models import UserAsk
 
 
 
-class AnotherUserForm(forms.ModelForm):
+class UserAskForm(forms.ModelForm):
     class Meta:
         model = UserAsk
         fields = ['name','mobile','course_name']
--- apps/organization/urls.py
+++ apps/organization/urls.py
@@ -4,13 +4,13 @@ __date__ = '2017/6/11 15:58'
 
 from django.conf.urls import url,include
 
-from .views import OrgView
+from .views import OrgView,AddUserAskView
 
 
 urlpatterns = [
 
     # 课程机构首页
     url(r'^list/$', OrgView.as_view(), name="org_list"),
-
+    url(r'^add_ask/$', AddUserAskView.as_view(), name="add_ask"),
 
 ]
